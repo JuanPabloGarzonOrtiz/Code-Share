dc1804cd036580456aaabb69d2805dc996277cca
janpo <juangarzon371@gmail.com>
2025-04-03 15:10:27 -0500

    Actualizacion de Ruta de Servidor

--- database/db.sql
+++ database/db.sql
@@ -1,17 +1,19 @@
-USE bawwzoeizzuehatgliy3;
 /*Tablas*/
 CREATE TABLE users(
-	id_user INTEGER PRIMARY KEY AUTO_INCREMENT,
-	userName VARCHAR(39) NOT NULL,
-	userBio VARCHAR(160),
-	photoUser TEXT
+	id_user SERIAL PRIMARY KEY,
+	token TEXT NOT NULL,
+	display_name VARCHAR(255),
+	user_name VARCHAR(40) NOT NULL,
+	user_bio VARCHAR(160),
+	photo_user VARCHAR(80),
+	url_profile VARCHAR(60) NOT NULL
 );
+
 CREATE TABLE repositories(
-	id_repo INTEGER PRIMARY KEY AUTO_INCREMENT,
-	nameRepo VARCHAR(100) NOT NULL,
+	id_repo SERIAL PRIMARY KEY,
+	name_repo VARCHAR(100) NOT NULL,
 	description TEXT,
-	urlPage TEXT NOT NULL,
-	screemshots JSON NOT NULL,
+	url_page TEXT NOT NULL,
 	id_user INTEGER NOT NULL,
 	FOREIGN KEY(id_user) REFERENCES users(id_user)
 );
