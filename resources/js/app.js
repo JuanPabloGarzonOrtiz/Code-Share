6f375597f81750ab345407d69e239bc46f77c30a
janpo <juangarzon371@gmail.com>
2025-04-04 14:30:46 -0500

    actualizacion de usuarios en la GUI con el Servidor

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,17 @@
 window.addEventListener("DOMContentLoaded", () =>{
+    let section = document.getElementById("muro");
     fetch("https://server-5lrb.onrender.com/dataUser")
-    .then(response => response.text())
+    .then(response => response.json())
     .then(data => {
-        document.getElementById("name_User").innerHTML = data;
+        data.forEach(usuario => {
+            section.insertAdjacentHTML("beforeend",
+                                        `<div class="perfil">
+                                            <a href="profile.html">${usuario.photo_user}1</a>
+                                            <div>
+                                                <h1>${usuario.display_name}</h1>
+                                                <p>${usuario.user_bio} </p>
+                                            </div>
+                                        </div>`);
+        });
     })
 });
