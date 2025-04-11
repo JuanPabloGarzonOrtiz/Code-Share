4db8fadf91a2495f8e9fdef2fc7d6ef4cc14f387
janpo <juangarzon371@gmail.com>
2025-04-11 00:06:41 -0500

    integracion de botones de desplzamiento de imagenes en publicaciones

--- resources/js/requestsServer.js
+++ resources/js/requestsServer.js
@@ -32,7 +32,29 @@ function insertHTML(ruta_Server, contenedor, position, htmlTemplate){
         }
     })
 }
+function move_imgs(){
+    setTimeout(() => {
+        const proyectos = document.querySelectorAll('.proyect');
+        proyectos.forEach(proyect => {
+            const btnLeft = proyect.querySelector('.left')
+            const btnRight = proyect.querySelector('.right')
+            const container = proyect.querySelector('.section_imgs')
 
+            if(btnLeft && btnRight && container){
+                const imageWidth = container.querySelector('img').offsetWidth;
+                const gap = parseInt(getComputedStyle(container).gap) || 0;
+                const totalMove = imageWidth + gap;
+
+                btnLeft.addEventListener('click', () => {
+                    container.scrollBy({ left: -totalMove, behavior: 'smooth' });
+                });
+                btnRight.addEventListener('click', () => {
+                    container.scrollBy({ left: totalMove, behavior: 'smooth' });
+                });
+            }
+        })
+    }, 500)
+}
 window.addEventListener("DOMContentLoaded", () => {
     insertHTML(
         "https://server-5lrb.onrender.com/dataRepo",
@@ -47,18 +69,29 @@ window.addEventListener("DOMContentLoaded", () => {
                     <img class="photo_profile" src="\${photo_user}">
                 </a>
             </div>
-            <div class="images-move">
-                <svg width="100" height="100">
-                    <path d="M58.333,75.000L33.333,50.000L58.333,25.000L64.167,30.833L45.000,50.000L64.167,69.167L58.333,75.000ZZ" style="fill: rgb(255, 255, 255);" class="fills"/>
-                </svg>
-                <img class="image_muro" src="resources/images/Index.png" alt="">
-                <svg width="100" height="100">
-                    <path d="M52.500,50.000L33.333,30.833L39.167,25.000L64.167,50.000L39.167,75.000L33.333,69.167L52.500,50.000ZZ" style="fill: rgb(255, 255, 255);" class="fills"/>
-                </svg>
+            <div class="container">
+                <button class="left">                
+                    <svg width="60" height="100" viewBox="25 10 40 70">
+                        <path d="M58.333,75.000L33.333,50.000L58.333,25.000L64.167,30.833L45.000,50.000L64.167,69.167L58.333,75.000ZZ" style="fill: rgb(255, 255, 255);" class="fills"/>
+                    </svg>
+                </button>
+                <div class="section_imgs">
+                    <img class="image_muro" src="resources/images/default_image.jpg" alt="">
+                    <img class="image_muro" src="resources/images/default_image.jpg" alt="">
+                    <img class="image_muro" src="resources/images/default_image.jpg" alt="">
+                    <img class="image_muro" src="resources/images/default_image.jpg" alt="">
+                </div>
+                <button class="right">                
+                    <svg width="60" height="100" viewBox="25 10 40 70">
+                        <path d="M52.500,50.000L33.333,30.833L39.167,25.000L64.167,50.000L39.167,75.000L33.333,69.167L52.500,50.000ZZ" style="fill: rgb(255, 255, 255);" class="fills"/>
+                    </svg>
+                </button>
             </div>
             <p>\${description}</p>
         </div>`
     );
+    move_imgs();
+    
     insertHTML(
         "https://server-5lrb.onrender.com/dataUser",
         "muro-search",
@@ -78,7 +111,9 @@ window.addEventListener("DOMContentLoaded", () => {
         "main-profile",
         "afterbegin",
         `<section class="Presentacion" id="presentacion">
-            <img src="\${photo_user}">
+            <a href="https://github.com/\${user_name}">
+                <img src="\${photo_user}">
+            </a>
             <div>
                 <h1>\${display_name}</h1>
                 <p>\${user_bio}</p>
@@ -95,9 +130,9 @@ window.addEventListener("DOMContentLoaded", () => {
             </a>
             <div class="info_proyect">
                 <div class="section-desplazable">
-                    <img  class="image_muro" src="../images/Index.png" alt="">
-                    <img  class="image_muro" src="../images/Index.png" alt="">
-                    <img  class="image_muro" src="../images/Index.png" alt="">
+                    <img  class="image_muro" src="../images/default_image.jpg" alt="">
+                    <img  class="image_muro" src="../images/default_image.jpg" alt="">
+                    <img  class="image_muro" src="../images/default_image.jpg" alt="">
                 </div>
                 <p>\${description}</p>
             </div>
